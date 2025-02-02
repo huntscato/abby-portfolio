2b539308058f5cffc7c256f149a4b12607fb78bd
Hunter <hgouldthorpe95@gmail.com>
2025-02-01 21:35:36 -0500

    2/1/2025

--- js/main.js
+++ js/main.js
@@ -6,27 +6,3 @@ document.addEventListener('DOMContentLoaded', () => {
       menu.classList.toggle('active');
     });
   });
-
-  const scrollContainer = document.querySelector('.scroll-container'); 
-
-  const photos = [
-    'img/abigail-underwater.png',
-    'img/crystal-ball.png',
-    'img/mushroom.png',
-    'img/nature.png',
-    'img/new-years-eve.png',
-    'img/old-flames.png',
-    'img/rocks.png',
-    'img/secret-garden.png'
-  ];
-
-  function addPhotosToGallery() {
-    photos.forEach(photo => {
-      const img = document.createElement('img');
-      img.src = photo;
-      img.alt = 'Photo fromt the gallery';
-      scrollContainer.appendChild(img);
-    });
-  }
-
-  addPhotosToGallery();
