028740083373d8232023960bad025f74109cd8f5
Hunter <hgouldthorpe95@gmail.com>
2025-02-21 20:13:07 -0500

    Adjusted logo size in header and began testimonials page

--- js/homepage.js
+++ js/homepage.js
@@ -1,4 +1,4 @@
-/**** HOMEPAGE ****/
+
 
 document.getElementById('menu-toggle').addEventListener('click', function() {
     document.getElementById('dropdown-menu').classList.toggle('active');
---
+++ js/testimonials.js
@@ -0,0 +1,4 @@
+document.getElementById('menu-toggle').addEventListener('click', function() {
+    document.getElementById('dropdown-menu').classList.toggle('active');
+});
+
